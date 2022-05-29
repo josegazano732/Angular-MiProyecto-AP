bc6081bcce7a34e32dd25bc34fcf4035b27a68d7
josegazano732 <josegazano7@outlook.com>
2022-05-29 11:25:33 -0300

    JLG

--- src/app/componentes/inicio-sesion/auth.service.ts
+++ src/app/componentes/inicio-sesion/auth.service.ts
@@ -54,7 +54,7 @@ import { Usuario } from './usuario';
     params.set('grant_type', 'password');
     params.set('username', usuario.username);
     params.set('password', usuario.password);
-    console.log(params.toString());
+    //console.log(params.toString());
 
       return this.http.post<any>(urlEndpoint, params.toString(), { headers: httpHeaders })
     }
